371814b4088fafc7f6a2ccd3fa0b791c06c0983f
Meryem <meryem.ozcelik@gatech.edu>
2019-06-22 15:41:37 -0400

    setting up express server using morgan serving static files

--- node-express/index.js
+++ node-express/index.js
@@ -4,7 +4,12 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
+const morgan = require('morgan');
+
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   console.log(req.headers);
