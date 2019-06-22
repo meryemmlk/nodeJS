58c2aeaf43bd6b535897ad4151535d82aaa1c2c6
Meryem <meryem.ozcelik@gatech.edu>
2019-06-22 13:35:49 -0400

    callback and error handling example

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,11 +1,12 @@
 var rect = require('./rectangle');
-/*
+/* 1
 var rect = {
 	perimeter: (x, y) => (2*(x+y)),
 	area: (x, y) => (x*y)
 };
 */
 
+/* 2
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
@@ -18,6 +19,24 @@ function solveRect(l,b) {
 	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
     }
 }
+*/
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
 solveRect(2,4);
 solveRect(3,5);
