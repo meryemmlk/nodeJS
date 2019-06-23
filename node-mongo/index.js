7b095decb427cd5be1d360885bef6a580d2381e3
Meryem <meryem.ozcelik@gatech.edu>
2019-06-22 23:57:56 -0400

    promises used

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,8 +5,47 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'm3cafe';
 
+MongoClient.connect(url).then((client) => {
+
+    console.log('Connected correctly to server');
+    const db = client.db(dbname);
+
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
+
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
+
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+                            
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
 
 
+/* before promises, just callbacks
 MongoClient.connect(url, (err, client) => {
 
     assert.equal(err,null);
@@ -66,4 +105,4 @@ MongoClient.connect(url, (err, client) => {
     */
    /*********************************************/ 
 
-});
+//});
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,3 +1,30 @@
+/* with promises */
+const assert = require('assert');
+
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.insert(document);
+};
+
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.find({}).toArray();
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
+};
+
+
+
+
+/* before using promises
 const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
@@ -35,3 +62,4 @@ exports.updateDocument = (db, document, update, collection, callback) => {
         callback(result);        
     });
 };
+*/
